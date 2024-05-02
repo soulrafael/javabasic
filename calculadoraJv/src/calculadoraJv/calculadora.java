40cf7a440eb22507797ea813388d2b9724f55667
soulrafael <168490825+soulrafael@users.noreply.github.com>
2024-05-01 23:45:26 -0300

    Add files via upload
    Correção calcularora em java

---
+++ calculadoraJv/src/calculadoraJv/calculadora.java
@@ -0,0 +1,47 @@
+package calculadoraJv;
+
+import java.util.Scanner;
+
+public class calculadora {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!Calculadora Basica");
+	
+
+		// variaveis de entrada e instanciamento 
+				Scanner entrada=new Scanner(System.in);
+				System.out.println("Entre com o primeiro valor:");
+				int numero1=entrada.nextInt();
+				System.out.println("Entre com o segundo valor:");
+				int numero2=entrada.nextInt();
+				System.out.println("Entre com a operação:");
+				String operador=entrada.next();
+				int resultado;
+				switch(operador){
+					case"+":
+						resultado=numero1+numero2;
+						System.out.println(resultado);
+						break;
+					case"-":
+						resultado=numero1-numero2;
+						System.out.println(resultado);
+						break;
+					case"*":
+						resultado=numero1*numero2;
+						System.out.println(resultado);
+						break;
+					case"/":
+						resultado=numero1/numero2;
+						System.out.println(resultado);
+						break;
+					case"%":
+						resultado=numero1%numero2;
+						System.out.println(resultado);
+						break;
+				default:
+					System.out.println("Nenhuma OPeraÇÂo Seleciona");
+				}
+
+	}
+
+}
